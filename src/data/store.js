f8dce5b794d8563ecb9caf4370942c97f3d59cd5
ebenezerdon <ebenezerdonu@gmail.com>
2022-07-28 17:40:11 +0100

    add 02_02 end state

---
+++ src/data/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+const store = configureStore({
+  reducer: {},
+})
+
+export default store
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
 import App from './App'
+import store from './data/store'
 
 const container = document.getElementById('root')
 const root = createRoot(container)
-root.render(<App />)
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+)
