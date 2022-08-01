868f79c5bd7b54f924c6e720bc44d70dfd0de8ce
ebenezerdon <ebenezerdonu@gmail.com>
2022-08-01 16:27:29 +0100

    display cart products in cart page and length in navbar

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,35 +1,49 @@
+import { useSelector, useDispatch } from 'react-redux'
+import cartSlice from '../data/cartSlice'
 import productList from '../data/productList.json'
 import '../styles/cart.scss'
 
 const Cart = () => {
+  const { cartProductIds } = useSelector((state) => state.cart)
+  const cartProductData = productList.products.filter((product) => cartProductIds.includes(product.id))
+
+  const { removeFromCart, clearAllItems } = cartSlice.actions
+  const dispatch = useDispatch()
+
   return (
     <div className="cart">
-      <div className="cart-product">
-        <h3 className="header">Items in cart</h3>
-        {productList.products.map((product) => (
-          <div key={product.id} className="row">
-            <img className="item-image" src={product.imageUrl} alt="product" />
+      {cartProductData.length > 0 && (
+        <div className="cart-product">
+          <h3 className="header">Items in cart</h3>
+          {cartProductData.map((product) => (
+            <div key={product.id} className="row">
+              <img className="item-image" src={product.imageUrl} alt="product" />
 
-            <div className="item-info">
-              <h4>{product.name}</h4>
-              <p className="text-truncate">{product.detail}</p>
-              <button className="btn btn-primary">
-                <i className="bi bi-trash-fill" /> Remove Item
-              </button>
+              <div className="item-info">
+                <h4>{product.name}</h4>
+                <p className="text-truncate">{product.detail}</p>
+                <button className="btn btn-primary" onClick={() => dispatch(removeFromCart(product.id))}>
+                  <i className="bi bi-trash-fill" /> Remove Item
+                </button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
 
-        <footer className="text-center">
-          <button className="btn btn-primary">CHECKOUT</button>
-        </footer>
-      </div>
+          <footer className="text-center">
+            <button className="btn btn-primary" onClick={() => dispatch(clearAllItems())}>
+              CHECKOUT
+            </button>
+          </footer>
+        </div>
+      )}
 
-      <div className="text-center empty-cart">
-        <i className="bi bi-cart3" />
-        <p>Your cart is empty.</p>
-        <p>You have not added any item to your cart.</p>
-      </div>
+      {cartProductData.length < 1 && (
+        <div className="text-center empty-cart">
+          <i className="bi bi-cart3" />
+          <p>Your cart is empty.</p>
+          <p>You have not added any item to your cart.</p>
+        </div>
+      )}
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
 import { NavLink } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 import '../styles/navbar.scss'
 
 const Navbar = () => {
+  const { cartProductIds } = useSelector((state) => state.cart)
+
   return (
     <nav className="navbar">
       <NavLink to="/" end>
@@ -10,7 +13,7 @@ const Navbar = () => {
 
       <NavLink to="/cart" title="cart" className={({ isActive }) => `${isActive && 'selected'}`}>
         <i className="bi bi-cart3" />
-        <sup className="cart-number">7</sup>
+        <sup className="cart-number">{cartProductIds.length}</sup>
       </NavLink>
 
       <NavLink to="/" className={({ isActive }) => (isActive ? 'selected' : '')} title="products" end>
