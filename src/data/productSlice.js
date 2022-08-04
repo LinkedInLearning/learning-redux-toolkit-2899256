99b56078e7f781ea5b5e2ba7b1784d33fc13ec8d
ebenezerdon <ebenezerdonu@gmail.com>
2022-08-04 13:14:36 +0100

    set up createAsyncThunk

---
+++ src/data/productSlice.js
@@ -0,0 +1,27 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+
+export const fetchAllProducts = createAsyncThunk('fetch-all-products', async (apiUrl) => {
+  const response = await fetch(apiUrl)
+
+  // console.log(await response.json())
+  return response.json()
+})
+
+const productSlice = createSlice({
+  name: 'products',
+  initialState: { data: [], fetchStatus: '' },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAllProducts.fulfilled, (state, action) => {
+        state.data = action.payload
+        state.fetchStatus = 'success'
+      })
+      .addCase(fetchAllProducts.pending, (state) => {
+        state.fetchStatus = 'loading'
+      })
+      .addCase(fetchAllProducts.rejected, (state) => {
+        state.fetchStatus = 'error'
+      })
+  },
+})
